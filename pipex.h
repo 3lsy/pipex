4fe7eb824dcb8c5c0175bb353731850e9763259f
echavez- <echavez-@student.42.fr>
2023-01-31 12:10:13 +0100

    [+]pipex.h

---
+++ pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 12:01:23 by echavez-          #+#    #+#             */
+/*   Updated: 2023/01/31 12:09:34 by echavez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		PIPEX_H
+# define	PIPEX_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include "libft.h"
+# include "ft_printf.h"
+
+#endif
