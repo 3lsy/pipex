c39abcc090222b315e2e506c17ee8c9c5ab7e959
3lsy <echavez-@student.42.fr>
2023-02-07 11:36:00 +0100

    [update] valgrind test

--- tests/valgrind_test.sh
+++ tests/valgrind_test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-valgrind --trace-children=yes --leak-check=full ./pipex infile "grep a1" "wc -l" outfile
+valgrind --trace-children=yes --leak-check=full --show-leak-kinds=all ./pipex infile "grep a1" "wc -l" outfile
